909693dc159d349c40a09a3f04da5a82b03c27e5
Jordan Smith <jordan.m.smith721@gmail.com>
2019-01-07 18:18:29 -0500

    Added validation for all forms and created order summary

--- index.php
+++ index.php
@@ -15,6 +15,66 @@ $flavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple B
     "caramel" => "Salted Caramel Cupcake", "velvet" => "Red Velvet", "lemon" => "Lemon Drop",
     "tiramsu" => "Tiramisu");
 
+
+if (!empty($_POST))
+{
+    $isValid = true;
+
+    //validate name
+    if(!empty($_POST['name']))
+    {
+        $name = $_POST['name'];
+    }
+    else
+    {
+        echo "<p>Please enter a name</p>";
+        $isValid = false;
+    }
+
+    //create valid flavors
+    $validFlavors = array();
+    foreach ($flavors as $key => $value)
+    {
+        $validFlavors[] = $value;
+
+    }
+    //validate that one box is checked
+    if(isset($_POST['flavors']))
+    {
+        $chosenFlavors = $_POST['flavors'];
+
+        foreach ($chosenFlavors as $chosenFlavor)
+        {
+            if(!in_array($chosenFlavor, $validFlavors))
+            {
+                echo "<p>Invalid Flavor</p>";
+                $isValid = false;
+            }
+        }
+    }
+    else
+    {
+        echo '<p>Please select at least one flavor</p>';
+        $isValid = false;
+    }
+
+    if($isValid)
+    {
+        $totalPrice = 0;
+
+        echo "<h2>Thank you $name for your order!</h2>
+                <ul>Order Summary:";
+        foreach ($chosenFlavors as $flavor)
+        {
+            echo"<li>$flavor</li>";
+            $totalPrice += 3.50;
+        }
+
+        echo"</ul><p>Order Total: $$totalPrice</p>";
+    }
+
+}
+
 ?>
 
 <!doctype html>
@@ -29,7 +89,7 @@ $flavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple B
 <body>
 <h1>Cupcake Order Form</h1>
 
-<form id="cupcake-form" method="post" action="">
+<form id="cupcake-form" method="POST" action="">
     <label>Name:</label>
     <input type="text" id="name" name="name" value='<?php echo"$name"?>'>
     <br>
@@ -39,13 +99,14 @@ $flavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple B
     //print checkboxes for all cupcake types
     foreach($flavors as $key => $value)
     {
-        echo "<input type='checkbox' name='$key' value='$value' id='$key'>$value<br>";
+        echo "<input type='checkbox' name='flavors[]' value='$value' id='$key'";
+        if(isset($_POST['flavors']) AND (in_array($value, $_POST['flavors']))) echo "checked";
+        echo">$value<br>";
     }
 
     ?>
 
     <input type="submit" id="submit">
-
 </form>
 
 
